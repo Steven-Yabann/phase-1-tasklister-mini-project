767773726db5e688dad77bfea644d83804887124
steven Yabann <stevekiptoo1@gmail.com>
2023-03-24 20:53:57 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form= document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    displayTodo(e.target.new_task_description.value)
+    form.reset()
+  })
+
 });
+
+function displayTodo(todo){
+  let list= document.createElement('li')
+  list.textContent= `${todo} `
+  let btn= document.createElement('button')
+  btn.textContent= 'x'
+  btn.addEventListener('click', deleteTask)
+  list.appendChild(btn)
+  let tasks= document.querySelector('#tasks')
+  tasks.appendChild(list)
+}
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
